0e73b0b37791194d61ccfe708fa42732b65f83ff
wallmonster <wmre2005@gmail.com>
2026-04-04 20:40:31 -0400

    Minor fixes

--- components/status_led/include/status_led.hpp
+++ components/status_led/include/status_led.hpp
@@ -2,17 +2,17 @@
 #include <stdint.h>
 
 namespace StatusLed {
-    /**
-     * @brief Initialize the WS2812 RGB LED on GPIO 48
-     */
-    void init();
+/**
+ * @brief Initialize the WS2812 RGB LED on GPIO 48
+ */
+void init();
 
-    /**
-     * @brief Set LED color with global brightness limit
-     * @param r Red (0-255)
-     * @param g Green (0-255)
-     * @param b Blue (0-255)
-     * @param brightness Percentage 0-100 (Default: 30 to protect eyes/OTG)
-     */
-    void set_color(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness = 30);
-}
+/**
+ * @brief Set LED color with global brightness limit
+ * @param r Red (0-255)
+ * @param g Green (0-255)
+ * @param b Blue (0-255)
+ * @param brightness Percentage 0-100 (Default: 30 to protect eyes/OTG)
+ */
+void set_color(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness = 10);
+} // namespace StatusLed
